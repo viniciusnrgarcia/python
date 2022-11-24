ce7fd501675356bd1d902a4d38e42f123c86485b
Vinicius N R Garcia <viniciusnrgarcia@gmail.com>
2022-11-24 20:33:39 -0300

    http test

---
+++ http/basic/example.py
@@ -0,0 +1,18 @@
+import requests
+import time
+
+cer = 'CERT.cer'
+requests.get("https://url", verify="CERT.cer")
+
+url = 'https://url.com/path'
+headers = {
+    'Content-Type': 'application/json',
+    'accept': 'application/json'
+}
+
+body = {'doc':123',id':'123'}
+
+response = requests.post(url=url, headers=headers, json=body, verify=cer)
+print(response.status_code, response.json())
+
+time.sleep(0.5)
